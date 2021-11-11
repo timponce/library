107ac659804bfbeca4efe2d6b6b5ab63c1924911
Timothy Ponce <timothyjponce@gmail.com>
2021-11-11 13:05:14 -0800

    Add and style input fields for book. Implement full window size and compact window size styles.

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+let bookEntry = document.getElementById('book-entry');
+let bookEntryCompact = document.getElementById('book-entry-compact');
+
 let myLibrary = [];
 
 function Book(title, author, pages, pagesRead, read) {
@@ -13,7 +16,31 @@ function addBookToLibrary(book) {
     myLibrary.push(book);
 };
 
-// function removeBookFromLibrary(book) {
-//     const index = array.indexOf
-//     myLibrary.
-// }
+function displayLibrary() {
+for (let i=0; i < myLibrary.length; i++) {
+    const newDiv = document.createElement('div');
+    const newContent = document.createTextNode('Title: ' + myLibrary[i].title + ' ')
+    newDiv.appendChild(newContent);
+    document.body.appendChild(newDiv);
+    console.log('success');
+}
+};
+
+window.addEventListener('resize', function() {
+    if (window.innerWidth < 900) {
+        bookEntry.style.display = 'none';
+        bookEntryCompact.style.display = 'flex';
+    } else if (window.innerWidth >= 900) {
+        bookEntry.style.display = 'flex';
+        bookEntryCompact.style.display = 'none';
+    }
+});
+
+//Manually entered placeholder books to have content for formatting.
+let book1 = new Book('Dune', 'Frank Herbet', '896', '896', 'Read');
+let book2 = new Book('Children of Dune', 'Frank Herbert', '745', '0', 'Not read');
+let book3 = new Book('Breakfast of Champions', 'Kurt Vonnegut', '323', '323', 'Read');
+
+addBookToLibrary(book1);
+addBookToLibrary(book2);
+addBookToLibrary(book3);
